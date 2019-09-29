c6490edaf7d9006d276e6224cf4a8e564a433584
Cogentx <cogentx@live.com>
2019-09-29 18:40:33 -0230

    checklists-L04-interface and data-service (w)

--- src/app/checklists/checklists.page.ts
+++ src/app/checklists/checklists.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { AlertController, IonList, NavController } from '@ionic/angular';
+import { ChecklistDataService } from '../services/checklist-data.service';
 
 @Component({
   selector: 'app-checklists',
@@ -7,25 +8,78 @@ import { AlertController, IonList, NavController } from '@ionic/angular';
   styleUrls: ['./checklists.page.scss'],
 })
 export class ChecklistsPage implements OnInit {
+  @ViewChild(IonList, { static: false}) slidingList: IonList;
 
   title = 'TITLE GOES HERE';
   numItems = 0;
 
-  constructor() { }
+  constructor(
+    public dataService: ChecklistDataService,
+    private alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
 
   addChecklist() {
-    console.log('addChecklist');
+    this.alertCtrl
+      .create({
+        header: 'New Checklist',
+        message: 'Enter the name of your new checklist below:',
+        inputs: [
+          {
+            type: 'text',
+            name: 'name'
+          }
+        ],
+        buttons: [
+          {
+            text: 'Cancel'
+          },
+          {
+            text: 'Save',
+            handler: data => {
+              this.dataService.createChecklist(data);
+            }
+          }
+        ]
+      })
+      .then(prompt => {
+        prompt.present();
+      });
   }
 
   renameChecklist(checklist) {
-    console.log('rename: ', checklist);
+    this.alertCtrl
+      .create({
+        header: 'Rename Checklist',
+        message: 'Enter the new name of this checklist below:',
+        inputs: [
+          {
+            type: 'text',
+            name: 'name'
+          }
+        ],
+        buttons: [
+          {
+            text: 'Cancel'
+          },
+          {
+            text: 'Save',
+            handler: data => {
+              this.dataService.renameChecklist(checklist, data);
+            }
+          }
+        ]
+      })
+      .then(prompt => {
+        prompt.present();
+      });
   }
 
   removeChecklist(checklist) {
-    console.log('remove: ', checklist);
+    this.slidingList.closeSlidingItems()
+      .then(() => {
+        this.dataService.removeChecklist(checklist);
+      });
   }
-
 }
--- src/app/services/checklist-data.service.ts
+++ src/app/services/checklist-data.service.ts
@@ -1,9 +1,98 @@
 import { Injectable } from '@angular/core';
+import { Checklist } from '../interfaces/checklists';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChecklistDataService {
+  public checklists: Checklist[] = [];
+  public loaded = false;
 
-  constructor() { }
+  constructor(private storage: Storage) {}
+
+  load(): Promise<boolean> {
+    return Promise.resolve(true);
+  }
+
+  save(): void {
+    this.storage.set('checklists', this.checklists);
+  }
+
+  createChecklist(data): void {
+    this.checklists.push({
+      id: this.generateSlug(data.name),
+      title: data.name,
+      items: [],
+    });
+
+    this.save();
+  }
+
+  renameChecklist(checklist, data): void {
+    const index = this.checklists.indexOf(checklist);
+
+    if (index > -1) {
+      this.checklists[index].title = data.name;
+      this.save();
+    }
+  }
+
+  removeChecklist(checklist): void {
+    const index = this.checklists.indexOf(checklist);
+
+    if (index > -1) {
+      this.checklists.splice(index, 1);
+      this.save();
+    }
+  }
+
+  getChecklist(id): Checklist {
+    return this.checklists.find(checklist => checklist.id === id);
+  }
+
+  addItem(checklistId, data): void {
+    this.getChecklist(checklistId).items.push({
+      title: data.name,
+      checked: false,
+    });
+
+    this.save();
+  }
+
+  removeItem(checklist, item): void {
+    const index = checklist.items.indexOf(item);
+
+    if (index > -1) {
+      checklist.items.splice(index, 1);
+      this.save();
+    }
+  }
+
+  renameItem(item, data): void {
+    item.title = data.name;
+    this.save();
+  }
+
+  toggleItem(item): void {
+    item.checked = !item.checked;
+    this.save();
+  }
+
+  generateSlug(title): string {
+    // NOTE: simplistic slug generabtor; not production grade.
+    let slug = title.toLowerCase().replace(/\s+/g, '-');
+
+    // Check if the slug already exists
+    const exists = this.checklists.filter(checklist => {
+      return checklist.id.substring(0, slug.length) === slug;
+    });
+
+    // If the title is already being used, add a number to make the slug unique
+    if (exists.length > 0) {
+      slug = slug + exists.length.toString();
+    }
+
+    return slug;
+  }
 }
